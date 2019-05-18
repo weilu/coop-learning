ec743a363bbe0791b8ba3e14eca12c980404da9f
Wei Lu <luwei.here@gmail.com>
2019-05-18 12:17:15 +0800

    Implement the top covering algorithm

--- src/top_covering.py
+++ src/top_covering.py
@@ -1,17 +1,46 @@
 from graph_tool.all import *
 
 
-def top_cover(pref):
-    graph, vlabel_to_index = build_graph(pref)
+def find_smallest_cc(graph):
     smallest_cc = None
-    smallest_cc_size = len(pref)
+    smallest_cc_size = graph.num_vertices()
     for v in graph.vertices():
         cc = graph_tool.topology.label_out_component(graph, v)
         cc_size = sum(cc.a)
         if cc_size < smallest_cc_size:
             smallest_cc_size = cc_size
             smallest_cc = cc
-    print(smallest_cc.a)
+    return frozenset([graph.vp.label[i] for i, v in enumerate(smallest_cc.a) if v == 1])
+
+
+def update_preferences(pref, smallest_cc):
+    new_pref = {}
+    for k, v in pref.items():
+        if k in smallest_cc:
+            continue
+        choice_set_set = set() # for deduplication
+        choice_set = []
+        num_choice_sets = len(choice_set_set)
+        for n in v:
+            new_n = frozenset(n - smallest_cc)
+            choice_set_set.add(new_n)
+            if num_choice_sets < len(choice_set_set):
+                num_choice_sets = len(choice_set_set)
+                choice_set.append(new_n)
+        new_pref[k] = choice_set
+    return new_pref
+
+
+def top_cover(pref):
+    stable_partition = []
+    while len(pref) > 1:
+        graph, vlabel_to_index = build_graph(pref)
+        smallest_cc = find_smallest_cc(graph)
+        stable_partition.append(smallest_cc)
+        pref = update_preferences(pref, smallest_cc)
+    if len(pref) == 1:
+        stable_partition.append(list(pref.values())[0][0])
+    print(stable_partition)
 
 
 # in: dict of player to a list of sets, each value is a player's choice set preference in descending order
@@ -31,6 +60,7 @@ def build_graph(pref):
     for i, player in enumerate(pref):
         vlabels[vlist[i]] = player
         vlabel_to_index[player] = i
+    g.vertex_properties['label'] = vlabels
 
     for k, v in pref.items():
         if len(v) < 1:
@@ -48,13 +78,21 @@ def build_graph(pref):
     return g, vlabel_to_index
 
 
+def freeze(pref):
+    frozen = {}
+    for k, v in pref.items():
+        frozen[k] = [frozenset(neighbors) for neighbors in v]
+    return frozen
+
+
 def generate_preferences():
-    return {
+    sample = {
         1: [{1, 2}, {1, 2, 3}, {1, 2, 4}, {1, 2, 3, 4}, {1, 3}, {1, 3, 4}, {1, 4}, {1}],
         2: [{2, 3}, {2, 3, 4}, {1, 2, 3}, {1, 2, 3, 4}, {2, 4}, {1, 2, 4}, {1, 2}, {2}],
         3: [{1, 3}, {1, 2, 3}, {1, 3, 4}, {1, 2, 3, 4}, {2, 3}, {2, 3, 4}, {3, 4}, {3}],
         4: [{3, 4}, {2, 3, 4}, {1, 3, 4}, {1, 2, 3, 4}, {2, 4}, {1, 2, 4}, {1, 4}, {4}]
     }
+    return freeze(sample)
 
 
 if __name__ == "__main__":
