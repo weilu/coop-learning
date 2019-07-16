d6b0dc2fb121a6bf627beda87ef57c149b547e70
Wei Lu <luwei.here@gmail.com>
2019-07-16 16:10:53 +0800

    script to convert pickle to csv

---
+++ src/pickle_to_votes.py
@@ -0,0 +1,36 @@
+import csv
+import pickle
+
+def load_pickle(pickle_file):
+    with open(pickle_file, 'rb') as pf:
+        return pickle.load(pf)
+
+
+if __name__ == '__main__':
+    #{<vote_id>: { "kmmbrs2votes": {<kmmbr_id>: <vote_result>}}
+    laws = load_pickle('data/laws.pickle')
+
+    #{<kmmbr_id>: {kmmbr_name: <kmmbr_name>,
+    #              faction_id: <faction_id>,
+    #              faction_name: <faction_name> }}
+    kmmbrs = load_pickle('data/kmmrs.pickle')
+
+    kmmbr_ids = set()
+    for _, wrapper in laws.items():
+        kmmbr_ids |= wrapper['kmmbrs2votes'].keys()
+
+    sorted_kmmbr_ids = sorted(kmmbr_ids)
+    sorted_kmmbr_names = [kmmbrs[i]['kmmbr_name'] for i in sorted_kmmbr_ids]
+
+    with open('data/votes_names.csv', 'w') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(['vote_id'] + sorted_kmmbr_names)
+
+        for vote_id, wrapper in laws.items():
+            row = [None]*len(sorted_kmmbr_ids)
+            for kmmbr_id, vote in wrapper['kmmbrs2votes'].items():
+                index = sorted_kmmbr_ids.index(kmmbr_id)
+                row[index] = vote
+            # assert(None not in row)
+            csv_writer.writerow([vote_id] + row)
+
