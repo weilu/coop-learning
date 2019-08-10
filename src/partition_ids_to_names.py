39b3aebeac8c472c901c354afb0c914c060ebd9b
Wei Lu <luwei.here@gmail.com>
2019-08-10 10:08:33 +0800

    Add a script for mapping coalition index string to names & parties

---
+++ src/partition_ids_to_names.py
@@ -0,0 +1,31 @@
+import csv
+import sys
+
+
+def partition_str_to_list(partition_str):
+    partition_index_strs = ('}), ' + partition_str[1:-1] + ', frozenset({').split('}), frozenset({')[1:-1]
+    return list(coalition.split(', ') for coalition in partition_index_strs)
+
+if __name__ == '__main__':
+    partition_str = sys.argv[1]
+
+    member_map = {}
+    with open('data/members.csv') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            party_initials = ''.join(filter(str.isupper, row['party name']))
+            member_map[row['index']] = party_initials + ' - ' + row['google translated name']
+
+
+    pi = partition_str_to_list(partition_str)
+    pi_names = []
+    for coal in pi:
+        coal_names = []
+        for index in coal:
+            coal_names.append(member_map[index])
+        pi_names.append(sorted(coal_names))
+
+    sorted_pi_names = sorted(pi_names, key=len, reverse=True)
+    for coal in sorted_pi_names:
+        print(coal)
+
