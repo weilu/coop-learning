36ede04f94cbf665734d61215b13d996853c9709
Wei Lu <luwei.here@gmail.com>
2019-08-15 21:07:32 +0800

    Add a test case to show that bottom responsive may not have unique core

--- src/test_bottom_avoiding.py
+++ src/test_bottom_avoiding.py
@@ -14,6 +14,18 @@ class TestBottomAvoiding(unittest.TestCase):
         self.assertTrue(frozenset({1, 2}) in pi)
         self.assertTrue(frozenset({3}) in pi)
 
+        # same game as above, reordered, different core produced
+        # so core is not unique
+        reordered_game = {}
+        reordered_game[2] = game[2]
+        reordered_game[3] = game[3]
+        reordered_game[1] = game[1]
+
+        pi = bottom_avoid(reordered_game)
+        self.assertEqual(len(pi), 2)
+        self.assertTrue(frozenset({2, 3}) in pi)
+        self.assertTrue(frozenset({1}) in pi)
+
         # TODO: add more test cases
 
 
