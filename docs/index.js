95e95fde3f95ee54184f3b239022a54df9ce86c9
Wei Lu <luwei.here@gmail.com>
2019-08-22 17:11:29 +0800

    Use a dropdown menu to select partition to visualise

--- docs/index.js
+++ docs/index.js
@@ -25,9 +25,12 @@ Plotly.d3.json('sankey.json', function(fig){
   const group_colors = party_list.map(color_fn)
 
   // group by partition
-  var partition = [[72], [132, 137, 138, 141, 14, 54], [145, 140, 86], [64, 134], [129, 2, 3, 4, 130, 6, 7, 131, 133, 136, 11, 15, 16, 17, 18, 23, 25, 27, 29, 30, 32, 33, 36, 39, 43, 44, 45, 48, 49, 51, 52, 57, 58, 59, 60, 61, 62, 63, 65, 87, 96, 98, 99, 100, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126], [144, 139, 135], [97, 34, 37, 8, 9, 10, 46, 114, 21, 22, 55, 24, 26, 127, 94, 95], [146], [77], [0, 1, 128, 5, 12, 13, 19, 20, 28, 31, 35, 38, 40, 41, 47, 50, 53, 56, 66, 67, 68, 69, 70, 71, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83, 84, 85, 88, 89, 90, 91, 92, 93, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113], [42], [142], [143]]
+  var partition1 = [[72], [132, 137, 138, 141, 14, 54], [145, 140, 86], [64, 134], [129, 2, 3, 4, 130, 6, 7, 131, 133, 136, 11, 15, 16, 17, 18, 23, 25, 27, 29, 30, 32, 33, 36, 39, 43, 44, 45, 48, 49, 51, 52, 57, 58, 59, 60, 61, 62, 63, 65, 87, 96, 98, 99, 100, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126], [144, 139, 135], [97, 34, 37, 8, 9, 10, 46, 114, 21, 22, 55, 24, 26, 127, 94, 95], [146], [77], [0, 1, 128, 5, 12, 13, 19, 20, 28, 31, 35, 38, 40, 41, 47, 50, 53, 56, 66, 67, 68, 69, 70, 71, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83, 84, 85, 88, 89, 90, 91, 92, 93, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113], [42], [142], [143]]
   // shift partition index to skip the first two columns of nodes
-  partition = partition.map(coal => coal.map(idx => idx + members.length * 2))
+  partition1 = partition1.map(coal => coal.map(idx => idx + members.length * 2))
+
+  var partition2 = [[64, 134], [2, 8, 9, 10, 11, 21, 22, 24, 26, 34, 37, 39, 44, 45, 46, 48, 52, 55, 57, 94, 95, 97, 114, 125, 127], [146], [131, 132, 135, 72, 137, 138, 139, 140, 141, 14, 144, 145, 54, 86], [77], [143], [0, 1, 128, 5, 12, 13, 19, 20, 28, 31, 35, 38, 40, 41, 47, 50, 53, 56, 66, 67, 68, 69, 70, 71, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83, 84, 85, 88, 89, 90, 91, 92, 93, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113], [42], [142], [129, 130, 3, 4, 133, 6, 7, 136, 15, 16, 17, 18, 23, 25, 27, 29, 30, 32, 33, 36, 43, 49, 51, 58, 59, 60, 61, 62, 63, 65, 87, 96, 98, 99, 100, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 126]]
+  partition2 = partition2.map(coal => coal.map(idx => idx + members.length * 2))
 
   const partition_link_source = Array(members.length).fill().map((e,i) => i + members.length)
   const partition_link_target = Array(members.length).fill().map((e,i) => i + members.length * 2)
@@ -45,7 +48,7 @@ Plotly.d3.json('sankey.json', function(fig){
       },
       label: members.concat(members).concat(members).concat(party_list),
       color: colors.concat(colors).concat(colors).concat(group_colors),
-      groups: group_values.concat(partition)
+      groups: group_values.concat(partition1)
     },
     link: {
       source: link_source.concat(partition_link_source),
@@ -62,7 +65,21 @@ Plotly.d3.json('sankey.json', function(fig){
     height: 4000,
     font: {
       size: 10
-    }
+    },
+    updatemenus: [{
+      active: 0,
+      buttons: [{
+          label: "Partition 1",
+          method: "restyle",
+          args: ["node.groups", [group_values.concat(partition1)]]
+        },
+        {
+          label: "Partition 2",
+          method: "restyle",
+          args: ["node.groups", [group_values.concat(partition2)]]
+        },
+      ]
+    }]
   }
 
   Plotly.plot('sankey', data, layout)
