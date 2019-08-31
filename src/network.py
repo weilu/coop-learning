35a3b97a289679f11f0e5212d193933fa8313a34
Wei Lu <luwei.here@gmail.com>
2019-08-31 11:44:48 +0800

    Block model that produced something sensible

--- src/network.py
+++ src/network.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
                     level=logging.INFO)
 
 def stochastic_block_model(g):
-    state_args = dict(recs=[g.ep.weight], rec_types=["discrete-binomial"])
+    state_args = dict(recs=[g.ep.weight], rec_types=["real-normal"])
     state = graph_tool.inference.minimize_blockmodel_dl(g, state_args=state_args)
     blocks = state.get_blocks()
     block_map = defaultdict(list)
@@ -34,10 +34,16 @@ def votes_to_graph(votes):
                 for y in range(x+1, len(players)):
                     key = frozenset([players[x], players[y]])
                     together_counter[key] += 1
+                    assert together_counter[key] < len(votes)
+        for for_player in groups[1]:
+            for against_player in groups[2]:
+                key = frozenset([for_player, against_player])
+                together_counter[key] -= 1
+
 
     edges = []
     for frozen_vertex_pair, weight in together_counter.items():
-        if weight < 4000:
+        if weight < 1000:
             continue
         vertex_pair = set(frozen_vertex_pair)
         edges.append([vertex_pair.pop(), vertex_pair.pop(), weight])
