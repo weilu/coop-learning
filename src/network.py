4669fc091a4b8bbc7553f0d717f702628f604f2b
Wei Lu <luwei.here@gmail.com>
2019-08-31 13:57:55 +0800

    Allow specifying max number of partitions in block model
    And generate all partitions with max part between 2 and 30

--- src/network.py
+++ src/network.py
@@ -7,9 +7,10 @@ import logging
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
                     level=logging.INFO)
 
-def stochastic_block_model(g):
+
+def stochastic_block_model(g, B_max=None):
     state_args = dict(recs=[g.ep.weight], rec_types=["real-normal"])
-    state = graph_tool.inference.minimize_blockmodel_dl(g, state_args=state_args)
+    state = graph_tool.inference.minimize_blockmodel_dl(g, B_max=B_max, state_args=state_args)
     blocks = state.get_blocks()
     block_map = defaultdict(list)
     for player, b in enumerate(blocks):
@@ -20,7 +21,7 @@ def stochastic_block_model(g):
     return pi
 
 
-def votes_to_graph(votes):
+def votes_to_graph(votes, allow_neg_edge=False):
     num_players = len(votes[0])
     together_counter = Counter()
     for i, bill in enumerate(votes):
@@ -35,26 +36,22 @@ def votes_to_graph(votes):
                     key = frozenset([players[x], players[y]])
                     together_counter[key] += 1
                     assert together_counter[key] < len(votes)
-        for for_player in groups[1]:
-            for against_player in groups[2]:
-                key = frozenset([for_player, against_player])
-                together_counter[key] -= 1
-
+        if allow_neg_edge:
+            for for_player in groups[1]:
+                for against_player in groups[2]:
+                    key = frozenset([for_player, against_player])
+                    together_counter[key] -= 1
 
     edges = []
     for frozen_vertex_pair, weight in together_counter.items():
-        if weight < 1000:
-            continue
         vertex_pair = set(frozen_vertex_pair)
         edges.append([vertex_pair.pop(), vertex_pair.pop(), weight])
 
     g = Graph(directed=False)
     g.add_vertex(num_players)
-
     edge_weights = g.new_edge_property("int")
     g.edge_properties["weight"] = edge_weights
     g.add_edge_list(edges, eprops=[edge_weights])
-
     return g
 
 
--- src/test_network.py
+++ src/test_network.py
@@ -1,5 +1,6 @@
 import unittest
-from network import votes_to_graph
+from network import votes_to_graph, stochastic_block_model
+from knesset_test import read_votes_and_player_data
 
 class TestNetwork(unittest.TestCase):
 
@@ -31,6 +32,20 @@ class TestNetwork(unittest.TestCase):
             else:
                 print(e)
                 self.assertFalse(True)
+
+
+    def test_knesset(self):
+        votes, _ = read_votes_and_player_data()
+        configs = [('real-normal', True), ('discrete-binomial', False)]
+        for distribution, allow_neg_edge in configs:
+            g = votes_to_graph(votes, allow_neg_edge)
+            filename = f'data/partitions_network_block_model_{distribution}.txt'
+            with open(filename, 'w') as f:
+                for k in range(2, 30):
+                    pi = stochastic_block_model(g, B_max=k)
+                    f.write(str(pi) + '\n')
+                    if k % 10 == 0:
+                        print(f'done {distribution} cluster size: {k}')
 
 
 if __name__ == '__main__':
