b7cefef5b76e65daf1987abb405e3e5720fbdec6
Wei Lu <luwei.here@gmail.com>
2019-09-17 16:34:41 +0800

    Attempt to PACify boolean hedonic games (not great)
    It's not really a PAC version of the boolean game because 
    we break tie of likes using size favouring larger groups and
    use SCC to find stable coalition to remove

--- src/boolean.py
+++ src/boolean.py
@@ -1,6 +1,13 @@
 from collections import defaultdict, Counter
 from itertools import combinations
+import random
+import logging
 from votes_to_game import read_votes_and_player_data
+from top_covering import largest_scc_from_pref
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    level=logging.INFO)
 
 
 def votes_to_pref_tables(votes):
@@ -9,21 +16,29 @@ def votes_to_pref_tables(votes):
     for row in votes:
         for_group = []
         against_group = []
+        others_group = [] # keep the likes and dislikes same shape as votes
         for i, vote in enumerate(row):
             if vote == 1:
                 for_group.append(i)
             elif vote == 2:
                 against_group.append(i)
+            else:
+                others_group.append(i)
         for i in for_group:
             likes[i].append(frozenset(for_group))
             dislike = frozenset(against_group + [i])
-            if likes[i][-1] != dislike: # avoid case where I both like and dislike to be by myself
-                dislikes[i].append(dislike)
+            if likes[i][-1] == dislike: # avoid case where I both like and dislike to be by myself
+                dislike = None
+            dislikes[i].append(dislike)
         for i in against_group:
             likes[i].append(frozenset(against_group))
             dislike = frozenset(for_group + [i])
-            if likes[i][-1] != dislike:
-                dislikes[i].append(dislike)
+            if likes[i][-1] == dislike:
+                dislike = None
+            dislikes[i].append(dislike)
+        for i in others_group:
+            likes[i].append(None)
+            dislikes[i].append(None)
     return likes, dislikes
 
 
@@ -31,8 +46,8 @@ def simplify_pref_tables(likes, dislikes):
     sl = {}
     sd = {}
     for i in likes.keys():
-        liked_groups = likes[i]
-        disliked_groups = dislikes[i]
+        liked_groups = list(c for c in likes[i] if c != None)
+        disliked_groups = list(c for c in dislikes[i] if c != None)
         like_counts = Counter()
         dislike_counts = Counter()
         for like in liked_groups:
@@ -68,6 +83,7 @@ def find_core(likes, dislikes):
 
     remaining_players = set(likes.keys())
     while potentially_stable_likes:
+        logging.info(f'{len(remaining_players)} players remaining')
         sorted_potential = sorted(potentially_stable_likes, key=len, reverse=True)
         stable_coalition = sorted_potential[0]
         stable_partition.add(stable_coalition)
@@ -79,7 +95,9 @@ def find_core(likes, dislikes):
                 updated_potentially_stable_likes.add(p)
         potentially_stable_likes = updated_potentially_stable_likes
 
+    logging.info(f'exhausted potentially_stable_likes')
     while remaining_players:
+        logging.info(f'{len(remaining_players)} players remaining')
         p = find_implicit_stable_coalition(all_likes, all_dislikes, remaining_players)
         if not p: # all singleton coalitions
             for i in remaining_players:
@@ -93,6 +111,62 @@ def find_core(likes, dislikes):
     return stable_partition
 
 
+def approximate_preferences(players, votes, B, likes, dislikes, sample_size):
+    S_prime_indexes = random.choices(range(len(votes)), k=sample_size)
+    sample_votes = list(votes[i] for i in S_prime_indexes)
+    filtered_likes = {}
+    filtered_dislikes = {}
+    for i in players:
+        filtered_likes[i] = list(likes[i][j] for j in S_prime_indexes)
+        filtered_dislikes[i] = list(dislikes[i][j] for j in S_prime_indexes)
+    logging.info('done filtering pref tables')
+    # TODO: do we really need dislikes at all?
+    filtered_likes, _ = simplify_pref_tables(filtered_likes, filtered_dislikes)
+    logging.info('done simplifying pref tables')
+    for i in players:
+        if i in B and B[i] == {i}: # already singleton, no need to check further
+            continue
+        if not filtered_likes[i]:
+            continue
+        old_coal_len = len(B[i]) if i in B else 0
+        max_coalition = filtered_likes[i][0]
+        if i not in B: # initialization round
+            B[i] = max_coalition
+        else:
+            B[i] &= max_coalition
+        if old_coal_len != len(B[i]):
+            logging.info(f'player {i}\'s coalition size changed: {old_coal_len} -> {len(B[i])}')
+
+
+def find_pac_core(votes, sample_size):
+    players = set(range(len(votes[0])))
+    stable_partition = set()
+
+    while players:
+        logging.info(f'{len(players)} players left')
+        B = {}
+        likes, dislikes = votes_to_pref_tables(votes)
+        logging.info('done constructing pref tables')
+        approximate_preferences(players, votes, B, likes, dislikes, sample_size)
+        for round_index in range(len(players)):
+            logging.info(f'round {round_index}')
+            approximate_preferences(players, votes, B, likes, dislikes, sample_size)
+        logging.debug(f'done approximating preferences')
+
+        largest_scc = largest_scc_from_pref(B)
+        stable_partition.add(largest_scc)
+        logging.info(f'players to remove: {largest_scc}')
+        players = players - largest_scc
+
+        # change removed players' votes to neutral
+        for i, row in enumerate(votes):
+            for j, vote in enumerate(row):
+                if j in largest_scc and vote in (1, 2):
+                    row[j] = None
+    return stable_partition
+
+
+# for partial preference case – consider any omitted coalition acceptable
 def find_implicit_stable_coalition(all_likes, all_dislikes, remaining_players):
     for i in range(len(remaining_players)):
         k = len(remaining_players) - i # num_players, ... , 2, 1
@@ -103,6 +177,11 @@ def find_implicit_stable_coalition(all_likes, all_dislikes, remaining_players):
 
 if __name__ == '__main__':
     votes, _ = read_votes_and_player_data()
+    logging.info('done reading votes data')
     likes, dislikes = votes_to_pref_tables(votes)
+    logging.info('done constructing pref tables')
     likes, dislikes = simplify_pref_tables(likes, dislikes)
+    logging.info('done simplifying pref tables')
+    pi = find_core(likes, dislikes)
+    print(pi)
 
--- src/test_boolean.py
+++ src/test_boolean.py
@@ -1,5 +1,14 @@
+import copy
+import logging
+import random
 import unittest
-from boolean import votes_to_pref_tables, simplify_pref_tables, find_core
+from boolean import votes_to_pref_tables, simplify_pref_tables, find_core, find_pac_core
+from knesset_test import print_partition_stats, calculate_partition_edit_distances_and_print_stats
+from votes_to_game import read_votes_and_player_data
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    level=logging.INFO)
 
 
 class TestBoolean(unittest.TestCase):
@@ -46,14 +55,14 @@ class TestBoolean(unittest.TestCase):
         likes, dislikes = votes_to_pref_tables(votes)
 
         self.assertEqual(len(likes), 3)
-        self.assertEqual(likes[0], [frozenset([0, 1])])
+        self.assertEqual(likes[0], [frozenset([0, 1]), None, None, None])
         self.assertEqual(likes[1], [frozenset([0, 1]), frozenset([1]), frozenset([1]), frozenset([1])])
-        self.assertEqual(likes[2], [frozenset([2]), frozenset([2])])
+        self.assertEqual(likes[2], [frozenset([2]), None, None, frozenset([2])])
 
         self.assertEqual(len(dislikes), 3)
-        self.assertEqual(dislikes[0], [frozenset([0, 2])])
-        self.assertEqual(dislikes[1], [frozenset([1, 2]), frozenset([1, 2])])
-        self.assertEqual(dislikes[2], [frozenset([0, 1, 2]), frozenset([1, 2])])
+        self.assertEqual(dislikes[0], [frozenset([0, 2]), None, None, None])
+        self.assertEqual(dislikes[1], [frozenset([1, 2]), None, None, frozenset([1, 2])])
+        self.assertEqual(dislikes[2], [frozenset([0, 1, 2]), None, None, frozenset([1, 2])])
 
 
     def test_simplify_pref_tables(self):
@@ -80,14 +89,14 @@ class TestBoolean(unittest.TestCase):
         })
 
         likes = {
-            0: [frozenset({0, 1})],
+            0: [frozenset({0, 1}), None, None, None],
             1: [frozenset({0, 1}), frozenset({1}), frozenset({1}), frozenset({1})],
-            2: [frozenset({2}), frozenset({2})]
+            2: [frozenset({2}), None, None, frozenset({2})]
         }
         dislikes = {
-            0: [frozenset({0, 2})],
-            1: [frozenset({1, 2}), frozenset({1, 2})],
-            2: [frozenset({0, 1, 2}), frozenset({1, 2})]
+            0: [frozenset({0, 2}), None, None, None],
+            1: [frozenset({1, 2}), None, None, frozenset({1, 2})],
+            2: [frozenset({0, 1, 2}), None, None, frozenset({1, 2})]
         }
         sl, sd = simplify_pref_tables(likes, dislikes)
         self.assertEqual(sl, {
@@ -141,6 +150,26 @@ class TestBoolean(unittest.TestCase):
         }
         pi = find_core(likes, dislikes)
         self.assertEqual(pi, {frozenset({0}), frozenset({1}), frozenset({2})})
+
+
+    def test_pac_knesset(self):
+        random.seed(42)
+        original_votes, _ = read_votes_and_player_data()
+        logging.info('done reading votes data')
+
+        sample_size = int(0.75 * len(original_votes))
+        partitions = []
+
+        with open('data/partitions_pac_likes_dislikes_10_runs.txt', 'w') as f:
+            for _ in range(10):
+                votes = copy.deepcopy(original_votes)
+                pi = find_pac_core(votes, sample_size)
+                f.write(str(pi) + '\n')
+                print(pi)
+                print_partition_stats(pi)
+                partitions.append(pi)
+
+        calculate_partition_edit_distances_and_print_stats(partitions)
 
 
     def test_knesset(self):
